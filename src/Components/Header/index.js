88bb64986ff90e20d06248643419f7fb7611cc8b
Fabs <fabiscg79@gmail.com>
2022-08-08 23:37:34 -0400

    fixing some resposive

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -6,9 +6,9 @@ export default function Header({ currentPage, handlePageChange }) {
         <div>
             <nav className="navbar navbar-expand-lg bg-outline-primary w3-top w3-hide-small w3-bar w3-white w3-center w3-padding w3-opacity-min w3-hover-opacity-off">
                 <div className="container-fluid">
+                <a class="navbar-brand" href="#">MED-SPA WELLNESS</a>
 
-
-                    <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false
+                    <button className="navbar-toggler collapse" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false
                     " aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
